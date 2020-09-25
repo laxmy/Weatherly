65e07051c3ba8fa8e49542e0705c38b50a1b3826
Lakshmy Subramaniam <lakshmysubramaniam@Lakshmys-MacBook-Pro.local>
2020-09-25 14:49:09 +0530

    Removing Redux and Axios for simplicity

--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import reducer from './store/reducers'
-
-
-const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <React.StrictMode> 
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux'
-import locations from './locations'
-
-export default combineReducers({ locations })
